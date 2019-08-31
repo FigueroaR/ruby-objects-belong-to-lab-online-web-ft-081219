5348af013d171ccd6e3cf49ce6a74a7e239cbe13
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-31 20:16:15 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+class Artist 
+  attr_accessor :name
+  
+  def self.create(name)
+    name = artist.new 
+    name
+   end 
+end 
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :title, :artist
+  
+  def self.create(title)
+    name = title.new 
+    name
+   end 
+end 
