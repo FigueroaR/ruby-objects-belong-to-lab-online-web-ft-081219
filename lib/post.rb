9529fcb3a0b9511623b0ec86bd44954135eb3580
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-02 18:33:06 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,5 @@
 class Post
+<<<<<<< HEAD
   attr_accessor :title, :author
   
   def self.create(title)
@@ -6,3 +7,13 @@ class Post
     name
    end 
 end 
+=======
+  attr_accessor :tittle, :author
+  
+  def self.create(tittle)
+    tittle = tittle.new 
+    tittle
+   end 
+end 
+
+>>>>>>> fa08584215fe629b20e8e263c22c5044250391ed
